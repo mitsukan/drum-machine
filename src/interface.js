6bf5c33085430d4b53049d2f2ad226c7b68df2ea
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-31 13:33:07 +0100

    update for last commit

---
+++ src/interface.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#header").load("drumhtml/header.html");
+  $("#drum1").load("drumhtml/drum1.html");
+  $("#drum2").load("drumhtml/drum2.html");
+  $("#drum3").load("drumhtml/drum3.html");
+  $("#drum4").load("drumhtml/drum4.html");
+});
