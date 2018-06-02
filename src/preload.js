899453e299e398a33d47e2d6f62c8a37502b90d9
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-06-02 11:12:45 +0100

    create preloader with fade function

---
+++ src/preload.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    window.onload = function () {
+    $('.preload').fadeOut(2000, function(){ $('.preload').remove(); } );
+    };
+});
+
+/* the below is working but not remocing the div element */
+
+// $(function(){
+//     $(".preload").fadeOut(3000, function() {
+//         $(".container").fadeIn(0);
+//     });
+// });
